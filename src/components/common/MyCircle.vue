5c585e54da44115328047cee26593485c94c5a22
Kasimba <kasimba666@gmail.com>
2023-05-23 18:37:01 +0300

    Circle

---
+++ src/components/common/MyCircle.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div class="MyCircle" :style="{backgroundColor: mycolor}">
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MyCircle",
+    components: {},
+    props: ['mycolor'],
+    data() {
+        return {}
+    },
+    computed: {},
+    methods: {},
+    mounted() {
+    },
+}
+</script>
+
+<style lang="scss">
+/****  MyCircle  ****/
+.MyCircle {
+  width: 50px;
+  height: 50px;
+  background-color: blue;
+  border-style: solid;
+  border-width: 2px;
+  border-color: red;
+  border-radius: 50%;
+}
+</style>
