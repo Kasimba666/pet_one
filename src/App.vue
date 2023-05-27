0f3be54f7db78bc5c9e733db69a610d12d306adc
Kasimba666 <kasimba666@gmail.com>
2023-05-27 09:14:18 +0300

    Inputs

--- src/App.vue
+++ src/App.vue
@@ -22,7 +22,7 @@
 }
 
 nav {
-  padding: 30px;
+  padding: 10px;
 
   a {
     font-weight: bold;
--- src/components/views/BlockTagsResearch.vue
+++ src/components/views/BlockTagsResearch.vue
@@ -1,28 +1,30 @@
 <template>
-    <div class="BlockTagsResearch">
+  <div class="BlockTagsResearch">
 
-        <button @click="print($event)">Принт</button>
+    <!--        <button @click="print($event)">Принт</button>-->
 
-        <!--    <select name="" id=""></select>-->
-        <input type="text" v-model.number="maxhorizontal">
-        <input type="text" v-model.number="maxvertical">
-        <div
-            class="container"
-            :class="{large}"
-
-            @click="large^=1"
-            v-for="vertical in maxvertical">
-<!--            <div v-for="horizontal in maxhorizontal">-->
-<!--                {{ makeHSL(vertical / maxvertical, horizontal / maxhorizontal) }}-->
-<!--            </div>-->
-            <MyCircle
-                :mybackcolor="makeHSL(vertical / maxvertical, horizontal / maxhorizontal)"
-                mybordercolor='black'
-                v-for="horizontal in maxhorizontal">
-
-            </MyCircle>
-        </div>
+    <div class="inputblock">
+      <input type="text" v-model.number="maxhorizontal">
+      x
+      <input type="text" v-model.number="maxvertical">
+      <label> = {{ fieldsize }}</label>
     </div>
+
+    <div
+        class="container"
+        :class="{large}"
+
+        @click="large^=1"
+        v-for="vertical in maxvertical">
+
+      <MyCircle
+          :mybackcolor="makeHSL(vertical / maxvertical, horizontal / maxhorizontal)"
+          mybordercolor='black'
+          v-for="horizontal in maxhorizontal">
+
+      </MyCircle>
+    </div>
+  </div>
 
 
 </template>
@@ -31,38 +33,35 @@
 import MyCircle from "@/components/common/MyCircle.vue";
 
 export default {
-    name: "BlockTagsResearch",
-    components: {MyCircle},
-    props: [],
-    data() {
-        return {
-            counter: 10,
-            large: false,
-            vertical: 1,
-            horizontal: 1,
-            maxvertical: 16,
-            maxhorizontal: 16,
-        }
+  name: "BlockTagsResearch",
+  components: {MyCircle},
+  props: [],
+  data() {
+    return {
+      counter: 10,
+      large: false,
+      vertical: 1,
+      horizontal: 1,
+      maxvertical: 8,
+      maxhorizontal: 8,
+    }
+  },
+  computed: {
+    fieldsize: function () {
+      return this.maxvertical * this.maxhorizontal
     },
-    computed: {
-        backgroundcolors_size: function () {
-            return this.backgroundcolors.length
-        },
-        bordercolors_size: function () {
-            return this.backgroundcolors.length
-        },
-    },
+  },
     methods: {
-        print(e) {
-            console.log(e)
-        },
-        makeHSL: function(h, s) {
-            return 'hsl(' + Math.round(h*360) + ', ' + Math.round(s*100) + '%, 50%)'
-        },
+      print(e) {
+        console.log(e)
+      },
+      makeHSL: function (h, s) {
+        return 'hsl(' + Math.round(h * 360) + ', ' + Math.round(s * 100) + '%, 50%)'
+      },
     },
     mounted() {
     },
-}
+  }
 </script>
 
 <style lang="scss">
@@ -70,21 +69,22 @@ export default {
 .BlockTagsResearch {
   width: 100%;
   height: auto;
+  position: relative;
 
+  home {
+    position: relative;
+
+  }
   .container {
-
-    --circleSize: 80px;
+    --circleSize: 60px;
     display: flex;
-
     justify-content: center;
     //justify-content: space-around;
     align-items: center;
-
     &.large {
-      --circleSize: 180px;
+      --circleSize: 100px;
     }
   }
-
   //.container div {
   //  background: beige;
   //  color: hsl(34, 78%, 91%);
@@ -92,10 +92,23 @@ export default {
   //  padding: 10px;
   //}
   .MyCircle {
-    box-shadow:
-        //112px 2px 10px -1px hsl(28, 100%, 57%), 12px 32px 10px -1px currentColor,
-    inset 2px 2px 5px 0px hsl(240, 100%, 57%);
+    box-shadow: //112px 2px 10px -1px hsl(28, 100%, 57%), 12px 32px 10px -1px currentColor,
+        inset 2px 2px 5px 0px hsl(240, 100%, 57%);
     flex: 0 0 auto;
+  }
+
+  .inputblock {
+    //    opacity: 50%;
+    position: relative;
+    padding: 8px 8px 8px;
+    background: azure;
+  }
+  .inputblock input {
+    width: 20px;
+    background: azure;
+  }
+  .inputblock input:hover {
+    background-color: white;
   }
 
 }
