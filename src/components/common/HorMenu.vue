c8552af9907dae351bba05c8867dd1d643811e1f
Kasimba <kasimba666@gmail.com>
2023-06-10 17:32:35 +0300

    Stop propagation

--- src/components/common/HorMenu.vue
+++ src/components/common/HorMenu.vue
@@ -1,42 +1,47 @@
 <template>
-  <div class="HorMenu">
-    <div class="main-menu-item" v-for="mainmenuItem in menuItems"
-         @click="itemClick(mainmenuItem)">
-      {{ mainmenuItem.title}}
-      <div class="dropdown1-menu" v-if="mainmenuItem.children.length">
-        <div class="dropdown1-menu-item" v-for="dropdown1menuItem in mainmenuItem.children"
-             @click="itemClick(dropdown1menuItem)">
-          {{ dropdown1menuItem.title }}
-          <div class="dropdown2-menu" v-if="dropdown1menuItem.children.length">
-            <div class="dropdown2-menu-item" v-for="dropdown2menuItem in dropdown1menuItem.children"
-                 @click="itemClick(dropdown2menuItem)">
-              {{ dropdown2menuItem.title }}
+    <div class="HorMenu" :class="{'close-all':closeAll}">
+        <div class="main-menu-item" v-for="mainmenuItem in menuItems"
+             @click="itemClick(mainmenuItem, $event)">
+            {{ mainmenuItem.title }}
+            <div class="dropdown1-menu" v-if="mainmenuItem.children.length">
+                <div class="dropdown1-menu-item" v-for="dropdown1menuItem in mainmenuItem.children"
+                     @click="itemClick(dropdown1menuItem, $event)">
+                    {{ dropdown1menuItem.title }}
+                    <div class="dropdown2-menu" v-if="dropdown1menuItem.children.length">
+                        <div class="dropdown2-menu-item" v-for="dropdown2menuItem in dropdown1menuItem.children"
+                             @click="itemClick(dropdown2menuItem, $event)">
+                            {{ dropdown2menuItem.title }}
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "HorMenu",
-  components: {},
-  props: ['menuItems'],
-  data() {
-    return {
-
-    }
-  },
-  computed: {},
-  methods: {
-    itemClick(v) {
-      this.$emit('showItem', v);
+    name: "HorMenu",
+    components: {},
+    props: ['menuItems'],
+    data() {
+        return {
+            closeAll: false,
+        }
     },
-  },
-  mounted() {
-  },
+    computed: {},
+    methods: {
+        itemClick(v, e) {
+            e.stopPropagation();
+            this.$emit('showItem', v);
+            console.log(v, e);
+            this.closeAll=true;
+            setTimeout(()=>{this.closeAll=false;}, 300);
+            // this.$nextTick(()=>{this.closeAll=false;});
+        },
+    },
+    mounted() {
+    },
 }
 </script>
 
@@ -62,9 +67,16 @@ export default {
 
     &:hover {
       background-color: cornflowerblue;
+
       .dropdown1-menu {
         display: flex;
       }
+    }
+  }
+
+  &.close-all {
+    .main-menu-item:hover .dropdown1-menu {
+      display: none;
     }
   }
 
@@ -91,6 +103,7 @@ export default {
 
     &:hover {
       background-color: cornflowerblue;
+
       .dropdown2-menu {
         display: flex;
       }
